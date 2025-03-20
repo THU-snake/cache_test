731b51e1dc851bd6583533a5f4b3f7194a8156cb
THU-snake <154876777+THU-snake@users.noreply.github.com>
2025-03-20 16:55:56 +0800

    Update L2_model_sc.h

--- src/L2_model_sc.h
+++ src/L2_model_sc.h
@@ -3,6 +3,7 @@
 
 #include <systemc.h>
 #include "DEBUG_L2_model.h"
+#include "interfaces.h" // 包含 dcache_2_L2_memReq 和 L2_2_dcache_memRsp 的定义
 
 SC_MODULE(L2Cache) {
     // 端口定义
@@ -10,26 +11,22 @@ SC_MODULE(L2Cache) {
     sc_in<bool>       reset;        // 复位信号
     sc_in<dcache_2_L2_memReq> mem_req; // 内存请求
     sc_out<L2_2_dcache_memRsp> mem_rsp; // 内存响应
-    struct dcache_2_L2_memReq {
-    bool is_valid() const {
-        // 根据实际逻辑判断请求是否有效
-        return true; // 示例
-    }
-};
+
     // 内部信号
     sc_signal<bool>   req_valid;    // 请求有效信号
     sc_signal<bool>   rsp_ready;    // 响应就绪信号
+
     // L2 缓存实例
     DEBUG_L2_model l2_cache;
 
-    SC_CTOR(L2Cache) : l2_cache(1) {
+    SC_CTOR(L2Cache) : l2_cache("l2_cache", 1) { // 传递名称和 verbose_level
         // 注册进程
         SC_METHOD(process_mem_req);
-        sensitive << clk.pos();
+        sensitive << clk.posedge_event(); // 使用 posedge_event() 替代 pos()
         async_reset_signal_is(reset, true);
 
         SC_METHOD(process_mem_rsp);
-        sensitive << clk.pos();
+        sensitive << clk.posedge_event(); // 使用 posedge_event() 替代 pos()
     }
 
     // 进程：处理内存请求
@@ -57,4 +54,5 @@ SC_MODULE(L2Cache) {
         }
     }
 };
-#endif
+
+#endif // 
