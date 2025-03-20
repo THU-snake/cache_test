19f9de0ef488c5c93c4021d281d9bf6507aaa872
THU-snake <154876777+THU-snake@users.noreply.github.com>
2025-03-20 22:50:08 +0800

    Update interfaces.h

--- src/interfaces.h
+++ src/interfaces.h
@@ -94,10 +94,6 @@ struct dcache_2_L2_memReq : cache_building_block {
     //bool a_data;//only to indicate whether there is a data transaction
     cache_line_t a_data;
     
-    bool valid; // 假设有个标志表示有效性
-    // 其它成员...
-
-    bool is_valid() const { return valid; }
 
     bool operator==(const dcache_2_L2_memReq &other) const {
         // 根据具体的成员进行比较
